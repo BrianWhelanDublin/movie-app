f856f8dc89bec986968fde41b58a252e7c59c333
Brian Whelan <brian@friday.ie>
2022-08-31 18:21:02 +0100

    add types and requests

---
+++ src/requests/requests.ts
@@ -0,0 +1,5 @@
+const REQUESTS = {
+	trending: `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_IMDB_API_KEY}`,
+};
+
+export default REQUESTS;
---
+++ src/types/types.ts
@@ -0,0 +1,35 @@
+/**
+ * Media item type for Movie and Tv Series
+ */
+export interface MediaItem {
+	adult?: Boolean;
+	backdrop_path?: string;
+	st_air_date?: string;
+	genre_ids?: Array<number>;
+	id?: number;
+	media_type?: string;
+	name?: string;
+	origin_country?: Array<string>;
+	original_language?: string;
+	original_name?: string;
+	overview?: string;
+	popularity?: number;
+	poster_path?: string;
+	vote_average?: number;
+	vote_count?: number;
+	original_title?: string;
+	release_date?: string;
+	title?: string;
+	video?: Boolean;
+}
+
+/**
+ * Trending Api point retquest return
+ */
+
+export interface TrendingRequest {
+	page: number;
+	results: Array<MediaItem>;
+	total_pages: number;
+	total_results: number;
+}
